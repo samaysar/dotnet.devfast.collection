90c7536265afff34d9ad1722d6bbccf7fd90ee1a
samaysar <samay.sar@gmail.com>
2024-07-20 20:02:45 +0200

    init - 15

---
+++ src/DevFast.Net.Collection.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DevFast.Net.Collection.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
