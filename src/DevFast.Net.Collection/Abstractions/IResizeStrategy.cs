4769481e7d7484caa91010a05c27601133d33d95
samaysar <samay.sar@gmail.com>
2024-07-24 07:49:08 +0200

    heap continue - 4

---
+++ src/DevFast.Net.Collection/Abstractions/IResizeStrategy.cs
@@ -0,0 +1,19 @@
+﻿namespace DevFast.Net.Collection.Abstractions;
+
+/// <summary>
+/// Interface exposing sizing strategy for any collection that can be resized.
+/// </summary>
+public interface IResizeStrategy
+{
+    /// <summary>
+    /// Calculates the new size, based on the given value of current size.
+    /// </summary>
+    /// <param name="currentSize">Current size</param>
+    /// <param name="newSize">New size</param>
+    bool TryComputeNewSize(in long currentSize, out int newSize);
+
+    /// <summary>
+    /// Gets the truth value whether new size can be computed.
+    /// </summary>
+    bool CanResize { get; }
+}
