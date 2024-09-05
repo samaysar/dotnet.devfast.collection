c1784a416ebf699400d080c0bcfcbbc2cb8d800d
samaysar <samay.sar@gmail.com>
2024-09-05 13:25:50 +0200

    fast dico - 1

---
+++ src/DevFast.Net.Collection/Implementations/Concurrent/LookUps/FastDictionary.KeyEnumerable.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+
+namespace DevFast.Net.Collection.Implementations.Concurrent.LookUps;
+
+public sealed partial class FastDictionary<TKey, TValue>
+{
+    private class KeyEnumerable(FastDictionary<TKey, TValue> instance) : IEnumerable<TKey>
+    {
+        private readonly FastDictionary<TKey, TValue> _instance = instance;
+
+        public IEnumerator<TKey> GetEnumerator()
+        {
+            return new KeyEnumerator(_instance);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    private class KeyEnumerator : IEnumerator<TKey>
+    {
+        private readonly FastDictionary<TKey, TValue> _instance;
+        private int _currentPosition;
+        private IEnumerator<TKey>? _currentEnumerator;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public KeyEnumerator(FastDictionary<TKey, TValue> instance)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            _instance = instance;
+            Reset();
+        }
+
+        public bool MoveNext()
+        {
+#pragma warning disable CS8601 // Possible null reference assignment.
+            Current = default;
+#pragma warning restore CS8601 // Possible null reference assignment.
+            if (_currentEnumerator == null)
+            {
+                return false;
+            }
+
+            if (!_currentEnumerator.MoveNext())
+            {
+                while (AcquireNextEnumerator())
+                {
+                    if (!_currentEnumerator.MoveNext())
+                    {
+                        continue;
+                    }
+
+                    Current = _currentEnumerator.Current;
+                    return true;
+                }
+
+#pragma warning disable CS8601 // Possible null reference assignment.
+                Current = default;
+#pragma warning restore CS8601 // Possible null reference assignment.
+                return false;
+            }
+
+            Current = _currentEnumerator.Current;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _currentPosition = 0;
+            _currentEnumerator = ((IEnumerable<TKey>)[]).GetEnumerator();
+        }
+
+        public TKey Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            _currentEnumerator?.Dispose();
+        }
+
+        private bool AcquireNextEnumerator()
+        {
+            if (_instance.TryGetPartition(_currentPosition++, out Dictionary<TKey, TValue>? d))
+            {
+                Monitor.Enter(d);
+                try
+                {
+                    _currentEnumerator?.Dispose();
+                    _currentEnumerator = d.Keys.ToList().GetEnumerator();
+                }
+                finally
+                {
+                    Monitor.Exit(d);
+                }
+                return true;
+            }
+            else
+            {
+                _currentEnumerator?.Dispose();
+                _currentEnumerator = null;
+                return false;
+            }
+        }
+    }
+}
---
+++ src/DevFast.Net.Collection/Implementations/Concurrent/LookUps/FastDictionary.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace DevFast.Net.Collection.Implementations.Concurrent.LookUps;
+
+/// <summary>
+/// Represents a thread-safe collection of key-value pairs that can be accessed by multiple threads concurrently.
+/// </summary>
+/// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+/// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+public sealed partial class FastDictionary<TKey, TValue> :
+    IDictionary<TKey, TValue>,
+    IReadOnlyDictionary<TKey, TValue>
+        where TKey : notnull
+{
+    private readonly IEqualityComparer<TKey> _comparer;
+    private readonly Dictionary<TKey, TValue>[] _data;
+    private readonly int _concurrencyLevel;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FastDictionary{TKey, TValue}" /> class that is empty and
+    /// has the default initial capacity, has default concurrency level,
+    /// and uses default Equality comparer for the key.
+    /// <para>
+    /// NOTE: Total expected memory allocation is bit more than default_initial_capacity * default_concurrency_level.
+    /// </para>
+    /// </summary>
+    public FastDictionary() : this(EqualityComparer<TKey>.Default)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FastDictionary{TKey, TValue}" /> class that is empty and
+    /// has the default initial capacity, has default concurrency level,
+    /// and uses the <paramref name="comparer"/> for the key type.
+    /// <para>
+    /// NOTE: Total expected memory allocation is bit more than default_initial_capacity * default_concurrency_level.
+    /// </para>
+    /// </summary>
+    /// <param name="comparer">Equality comparer for the key</param>
+    public FastDictionary(IEqualityComparer<TKey> comparer) : this(0, comparer)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FastDictionary{TKey, TValue}" /> class that is empty
+    /// and has the given <paramref name="initialCapacity"/>, has given <paramref name="concurrencyLevel"/>
+    /// and uses default Equality comparer for the key.
+    /// <para>
+    /// NOTE: Total expected memory allocation is bit more than <paramref name="initialCapacity"/> * <paramref name="concurrencyLevel"/>.
+    /// </para>
+    /// NOTE: <paramref name="initialCapacity"/> has internal lower bound=0 and <paramref name="concurrencyLevel"/> has internal lower bound=2.
+    /// </summary>
+    /// <param name="initialCapacity">Initial estimated capacity</param>
+    /// <param name="concurrencyLevel">Concurrency level</param>
+    public FastDictionary(int initialCapacity,
+        int concurrencyLevel) : this(initialCapacity, concurrencyLevel, EqualityComparer<TKey>.Default)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FastDictionary{TKey, TValue}" /> class that is empty and
+    /// and has the given <paramref name="initialCapacity"/>, has default concurrency level,
+    /// and uses the <paramref name="comparer"/> for the key type.
+    /// <para>
+    /// NOTE: Total expected memory allocation is bit more than <paramref name="initialCapacity"/> * default_concurrency_level.
+    /// </para>
+    /// NOTE: <paramref name="initialCapacity"/> has internal lower bound=0.
+    /// </summary>
+    /// <param name="initialCapacity">Initial estimated capacity</param>
+    /// <param name="comparer">Equality comparer for the key</param>
+    public FastDictionary(int initialCapacity,
+        IEqualityComparer<TKey> comparer) : this(initialCapacity,
+        Environment.ProcessorCount,
+        comparer)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FastDictionary{TKey, TValue}" /> class that is empty
+    /// and has the given <paramref name="initialCapacity"/>, has given <paramref name="concurrencyLevel"/>
+    /// and uses the <paramref name="comparer"/> for the key type.
+    /// <para>
+    /// NOTE: Total expected memory allocation is bit more than <paramref name="initialCapacity"/> * <paramref name="concurrencyLevel"/>.
+    /// </para>
+    /// NOTE: <paramref name="initialCapacity"/> has internal lower bound=0 and <paramref name="concurrencyLevel"/> has internal lower bound=2.
+    /// </summary>
+    /// <param name="initialCapacity">Initial estimated capacity</param>
+    /// <param name="concurrencyLevel">Expected maximum concurrency</param>
+    /// <param name="comparer">Equality comparer for the key</param>
+    public FastDictionary(int initialCapacity,
+        int concurrencyLevel,
+        IEqualityComparer<TKey> comparer)
+    {
+        _comparer = comparer ?? EqualityComparer<TKey>.Default;
+        _concurrencyLevel = Math.Max(2, concurrencyLevel);
+        _data = new Dictionary<TKey, TValue>[_concurrencyLevel];
+        initialCapacity = Math.Max(0, initialCapacity);
+        for (int i = 0; i < _concurrencyLevel; i++)
+        {
+            _data[i] = new Dictionary<TKey, TValue>(initialCapacity, _comparer);
+        }
+    }
+
+    /// <inheritdoc />
+    public TValue this[TKey key]
+    {
+        get => TryGetValue(key, out TValue? v) ? v : throw new KeyNotFoundException();
+        set
+        {
+            Dictionary<TKey, TValue> d = GetPartition(key);
+            Monitor.Enter(d);
+            try
+            {
+                d[key] = value;
+            }
+            finally
+            {
+                Monitor.Exit(d);
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    public ICollection<TKey> Keys => throw new NotImplementedException();
+
+    /// <inheritdoc />
+    public ICollection<TValue> Values => throw new NotImplementedException();
+
+    /// <inheritdoc />
+    public int Count
+    {
+        get
+        {
+            int totCount = 0;
+            int i = _data.Length;
+            for (; i > 0;)
+            {
+                Dictionary<TKey, TValue> d = _data[--i];
+                Monitor.Enter(d);
+                try
+                {
+                    totCount += d.Count;
+                }
+                finally
+                {
+                    Monitor.Exit(d);
+                }
+            }
+            return totCount;
+        }
+    }
+
+    /// <inheritdoc />
+    public bool IsReadOnly => false;
+
+    /// <inheritdoc />
+    IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys => throw new NotImplementedException();
+
+    /// <inheritdoc />
+    IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values => throw new NotImplementedException();
+
+    /// <inheritdoc />
+    public void Add(TKey key, TValue value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public void Add(KeyValuePair<TKey, TValue> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public bool ContainsKey(TKey key)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public bool Remove(TKey key)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+#if NETCOREAPP3_0_OR_GREATER
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+    private Dictionary<TKey, TValue> GetPartition(TKey key)
+    {
+        unchecked
+        {
+            return _data[(uint)key.GetHashCode() % _concurrencyLevel];
+        }
+    }
+
+    private bool TryGetPartition(int position, [NotNullWhen(true)] out Dictionary<TKey, TValue>? partition)
+    {
+        partition = default;
+        if (position < 0 || position >= _concurrencyLevel)
+        {
+            return false;
+        }
+
+        partition = _data[position];
+        return true;
+    }
+}
