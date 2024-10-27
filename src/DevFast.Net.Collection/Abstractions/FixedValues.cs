2c629e964eb24b733eeec0aa60cfc431683e85f1
samaysar <samay.sar@gmail.com>
2024-10-27 12:26:55 +0100

    fast dico for release - 3

--- src/DevFast.Net.Collection/Abstractions/FixedValues.cs
+++ src/DevFast.Net.Collection/Abstractions/FixedValues.cs
@@ -1,23 +1,22 @@
-﻿namespace DevFast.Net.Collection.Abstractions
+﻿namespace DevFast.Net.Collection.Abstractions;
+
+/// <summary>
+/// A wrapper class holding constant values.
+/// </summary>
+public static class FixedValues
 {
     /// <summary>
-    /// A wrapper class holding constant values.
+    /// Minimum bound on initial capacity.
     /// </summary>
-    public static class FixedValues
-    {
-        /// <summary>
-        /// Minimum bound on initial capacity.
-        /// </summary>
-        public const int MinInitialCapacity = 0;
+    public const int MinInitialCapacity = 0;
 
-        /// <summary>
-        /// Minimum bound on concurrency level.
-        /// </summary>
-        public const int MinConcurrencyLevel = 2;
+    /// <summary>
+    /// Minimum bound on concurrency level.
+    /// </summary>
+    public const int MinConcurrencyLevel = 2;
 
-        /// <summary>
-        /// Maximum bound on concurrency level.
-        /// </summary>
-        public const int FastDictionaryMaxConcurrencyLevel = 256;
-    }
+    /// <summary>
+    /// Maximum bound on concurrency level.
+    /// </summary>
+    public const int FastDictionaryMaxConcurrencyLevel = 256;
 }
